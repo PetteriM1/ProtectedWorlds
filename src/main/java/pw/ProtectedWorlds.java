2acace0784e170e663fd562bfda29bc23a10e241
PetteriM1 <petteri.malkavaara1@gmail.com>
2018-12-27 19:23:00 +0200

    Initial commit

---
+++ src/main/java/pw/ProtectedWorlds.java
@@ -0,0 +1,69 @@
+package pw;
+
+import cn.nukkit.Player;
+import cn.nukkit.event.block.BlockIgniteEvent;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.block.BlockBreakEvent;
+import cn.nukkit.event.block.BlockPlaceEvent;
+import cn.nukkit.event.player.PlayerBucketEmptyEvent;
+import cn.nukkit.event.player.PlayerBucketFillEvent;
+import cn.nukkit.event.player.PlayerDropItemEvent;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+
+public class ProtectedWorlds extends PluginBase implements Listener {
+
+    Config config;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        config = getConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void action(BlockBreakEvent e) {
+        if (config.getStringList("worlds").contains(e.getBlock().getLevel().getName()) && !e.getPlayer().isOp() && config.getBoolean("noBlockBreaking")) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void action(BlockPlaceEvent e) {
+        if (config.getStringList("worlds").contains(e.getBlock().getLevel().getName()) && !e.getPlayer().isOp() && config.getBoolean("noBlockPlacing")) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void action(PlayerBucketFillEvent e) {
+        if (config.getStringList("worlds").contains(e.getPlayer().getLevel().getName()) && !e.getPlayer().isOp() && config.getBoolean("noBucketFilling")) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void action(PlayerBucketEmptyEvent e) {
+        if (config.getStringList("worlds").contains(e.getPlayer().getLevel().getName()) && !e.getPlayer().isOp() && config.getBoolean("noBucketEmptying")) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void action(PlayerDropItemEvent e) {
+        if (config.getStringList("worlds").contains(e.getPlayer().getLevel().getName()) && !e.getPlayer().isOp() && config.getBoolean("noItemDropping")) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void action(BlockIgniteEvent e) {
+        if (config.getStringList("worlds").contains(e.getBlock().getLevel().getName()) && config.getBoolean("noBlockIgniting") && e.getEntity() instanceof Player) {
+            if (!((Player) e.getEntity()).isOp()) {
+                e.setCancelled(true);
+            }
+        }
+    }
+}
